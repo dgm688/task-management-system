9ce588237405651b30434b1556ce8779acd96108
dgm688 <dgm.official688@gmail.com>
2026-06-14 00:14:54 +0300

    Simplify len checks in validation

--- task_manager/validation.py
+++ task_manager/validation.py
@@ -1,17 +1,17 @@
 from datetime import datetime
 
 def validate_task_title(title):
-    if len(title.strip()) == 0:
+    if len(title) == 0:
         raise ValueError("Error: Title cannot be empty.")
     return True
     
 def validate_task_description(description):
-    if len(description.strip()) == 0:
+    if len(description) == 0:
         raise ValueError("Error: Description cannot be empty.")
     return True
     
 def validate_due_date(due_date):
-    if len(due_date.strip()) == 0:
+    if len(due_date) == 0:
         raise ValueError("Error: Due date cannot be empty.")
     try:
         datetime.strptime(due_date, "%Y-%m-%d")
